e0434f1ed696de9c36b78143eb6d83ada0fd008e
gowthamjayakumar016 <gowthamjayakumar016@gmail.com>
2025-01-22 14:59:30 +0530

    New program

---
+++ CountEvendigits.java
@@ -0,0 +1,37 @@
+public class CountEvendigits {
+    public static void main(String[] args) {
+        System.out.println("Finding the even digits in the number");
+
+        int []arr= new int[]{
+            1,23,4,56,6778,5,4,345,
+        };
+
+        int k=Count(arr);
+        System.out.println("Number "+k);
+    }
+    static int Count (int arr[])
+    {
+        int count =0;
+        for (int i : arr) {
+            if (even(i))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    static boolean even(int num)
+    {
+        int count1=0;
+        while(num>0)
+        {
+            count1++;
+            num=num/10;
+        }
+        if(count1%2==0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
