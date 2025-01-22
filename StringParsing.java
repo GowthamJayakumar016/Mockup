16dd122a26ff33bad63b1457a82a0e4a78abe2ec
gowthamjayakumar016 <gowthamjayakumar016@gmail.com>
2025-01-22 13:51:36 +0530

    parsing string in the file

---
+++ StringParsing.java
@@ -0,0 +1,23 @@
+public class StringParsing {
+    public static void main(String[] args) {
+        System.out.println("Searching in the string");
+        String Str ="Hello";
+        char n ='p';
+        boolean o=Search( Str , n);
+        System.out.println(o);
+        
+    }
+    static boolean  Search(String Str ,char l)
+    {
+        for(int i=0;i<Str.length();i++)
+        {
+            if(l==Str.charAt(i))
+            {
+                return true;
+            }
+        }
+        return false;
+ 
+        
+    }
+}
