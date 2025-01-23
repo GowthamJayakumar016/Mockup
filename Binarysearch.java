013c21fb27975a1cc158154560225797c5adad4c
gowthamjayakumar016 <gowthamjayakumar016@gmail.com>
2025-01-23 12:50:56 +0530

    Binary Search is created

---
+++ Binarysearch.java
@@ -0,0 +1,34 @@
+public class Binarysearch {
+    public static void main(String[] args) {
+        System.out.println("Binary Search");
+        int []arr={1,2,3,4,5,6,7,8,9};
+        int k=binarysearch(arr, 9);
+        System.out.println(k);
+    }
+
+    static int binarysearch(int arr[],int target)
+    {
+        int start=0;
+        int end=arr.length-1;
+        
+        while(start<=end)
+        
+        {
+            int mid=(int)(end+start)/2;
+            if(arr[mid]==target)
+            {
+               return mid;
+            }
+            else if(target>arr[mid])
+            {
+                start=mid+1;
+                
+            }
+            else{
+                end=mid-1;
+            }
+
+        }
+        return -1;
+    }
+}
