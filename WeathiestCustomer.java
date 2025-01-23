f10c6414c0f4633f2d175284fe283feb74525829
gowthamjayakumar016 <gowthamjayakumar016@gmail.com>
2025-01-23 11:32:53 +0530

    new Leetcode problem

---
+++ WeathiestCustomer.java
@@ -0,0 +1,30 @@
+public class WeathiestCustomer {
+   public static void main(String[] args) {
+    System.out.println("finding the weathieast customer");
+
+    int [][] arr=new int[][]{
+        {1,23,4},
+        {45,57},
+        {2,455,7}
+    };
+    System.out.println("wealthieat customer "+Maxwealth(arr));
+
+   } 
+   static int  Maxwealth(int [][]arr){
+    int Maxwealth=0;
+    for (int[] a : arr) {
+        int sum=0;
+        for (int  is : a) {
+            sum+=is;
+        }
+        if(sum>Maxwealth)
+        {
+            Maxwealth=sum;
+        }
+    }
+    
+    
+    
+    return Maxwealth;
+   }
+}
