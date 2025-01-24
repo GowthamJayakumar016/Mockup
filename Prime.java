02ff3998b0bbf3b0f07cfaa6df769b9e2e07c320
gowthamjayakumar016 <gowthamjayakumar016@gmail.com>
2025-01-24 11:14:47 +0530

    Prime logic

---
+++ Prime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Prime {
+
+    public static void main(String[] args) {
+        
+        System.out.println("prime logic");
+        Scanner in =new Scanner(System.in);
+        int c=2;
+        System.out.println("Enter the Number to check  prime");
+        int num=in.nextInt();
+        in.close();
+        if(num==1||num==0)
+        {
+            System.out.println("neither prime nor composite");
+        }
+        else{
+            while(c<=(int)Math.sqrt(num))
+            {
+                if(num%c==0)
+                {
+                    System.out.println("Not a prime Number");
+                    return;
+                }
+                c++;
+            }
+            System.out.println("prime number");
+        }
+    }
+}
