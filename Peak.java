d5e628909b6e149482f3bf7fc7e8c6778dd9ef10
gowthamjayakumar016 <gowthamjayakumar016@gmail.com>
2025-01-26 12:42:49 +0530

    changes made"

---
+++ Peak.java
@@ -0,0 +1,31 @@
+// public class Peak {
+//     public static void main(String[] args) {
+//         System.out.println("Peak of the moubtain");
+//     }
+//     static int Peak(int arr[])
+//     {  int ans=0;
+//         int start=0;
+//         int end=arr.length-1;
+        
+//         while(start<=end)
+        
+//         {
+//             int mid=(int)(end+start)/2;
+//             if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1])
+//             {
+//                return arr[mid];
+//             }
+            
+//             else{
+              
+//             }
+
+//         }
+//         return ans;
+
+
+
+
+//         return ans;
+//     }
+// }
---
+++ SortFunc.java
@@ -0,0 +1,10 @@
+
+import java.util.Arrays;
+public class SortFunc {
+    public static void main(String[] args) {
+        System.out.println("Sorting an Array");
+        int arr[]={34,23,4,234,34};
+        Arrays.sort(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+}
