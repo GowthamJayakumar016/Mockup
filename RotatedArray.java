ec4a762055ea90304f83c18e222bc973515be087
gowthamjayakumar016 <gowthamjayakumar016@gmail.com>
2025-01-29 10:11:04 +0530

    values has been changed

--- RotatedArray.java
+++ RotatedArray.java
@@ -1,6 +1,6 @@
 public class RotatedArray {
     public static void main(String[] args) {
-        int []a=new int[]{3,4,5,6,7,0,1,2,3};
+        int []a=new int[]{1,2,3,4,5,6,7,8,9};
         System.out.println(FindPivot(a));
     }
     static int FindPivot(int []arr)
