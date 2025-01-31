1c1dc5f66111d1eed77af4b9775b0c61a823b94d
gowthamjayakumar016 <gowthamjayakumar016@gmail.com>
2025-01-31 20:21:34 +0530

    Cyclic sort

---
+++ Sorting Algorithms/CyclicSort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class CyclicSort {
+    public static void main(String[] args) {
+        int arr[]=new int[]{5,4,3,2,1};
+        System.out.println(Arrays.toString(Cyclicsort(arr)));
+    }
+
+    static int [] Cyclicsort(int arr[])
+    {
+        int i=0;
+        while(i<arr.length)
+        {
+            int CorrectIndex=arr[i]-1;
+            if(arr[i]!=arr[CorrectIndex])
+            {
+                int temp=arr[i];
+                    arr[i]=arr[CorrectIndex];
+                    arr[CorrectIndex]=temp;
+            }
+            else{
+                i++;
+            }
+        }
+        return arr;
+    }
+}
