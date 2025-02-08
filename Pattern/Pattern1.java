3f19384180742bf6e5d3792864c12ec4d531e63e
gowthamjayakumar016 <gowthamjayakumar016@gmail.com>
2025-02-08 13:09:04 +0530

    square pattern is added

--- Pattern/Pattern1.java
+++ Pattern/Pattern1.java
@@ -2,12 +2,22 @@ package Pattern;
 
 public class Pattern1 {
   public static void main(String[] args) {
-    Pattern(5);
+    SquarePattern(4);
   }
 static void Pattern(int n)
 {
 for (int  i = 0; i < n; i++) {
     for (int j = 0; j <=i; j++) {
+        System.out.print("* ");
+    }
+    System.out.println("");
+}
+}
+
+static void SquarePattern(int n)
+{
+for (int  i = 0; i < n; i++) {
+    for (int j = 0; j <=3; j++) {
         System.out.print("* ");
     }
     System.out.println("");
